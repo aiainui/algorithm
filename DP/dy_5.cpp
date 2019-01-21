72bc094cc6ab92d4bd7aca113b231c5aa4b99e42
dengyulong <dengyulong@100tal.com>
2019-01-22 01:45:24 +0800

    up

---
+++ DP/dy_5.cpp
@@ -0,0 +1,62 @@
+/*************************************************************************
+	> File Name: dy_5.cpp
+	> Author: dyl
+	> Mail: denyulong@100tal.com
+	> Created Time: Tue 22 Jan 2019 01:28:20 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+const int ms =  1000;
+int r[ms][ms], m[ms][ms],s[ms][ms];
+int n;
+void rent()
+{
+    int i,j,k,d;
+    for(d=3; d <= n; d++){
+        for(i=1; i <= n-d+1; i++){
+            j = i+d-1;
+            for(k=i+1; k<j; k++){
+                int temp;
+                temp = m[i][k] + m[k][j];
+                if(temp < m[i][j]){
+                    m[i][j] = temp;
+                    s[i][j] = k;
+                }
+            }
+        }
+    }
+}
+
+void print(int i, int j)
+{
+    if(s[i][j] == 0){
+        cout << "---" <<j;
+        return ;
+    }
+    print(i, s[i][j]);
+    print(s[i][j], j);
+}
+
+int main()
+{
+    int i, j;
+    cout << "请输入站点个数：";
+    cin >> n;
+    cout << "请依次输入各站点的租金：";
+    for(i = 1; i <= n; i++){
+        for(j = i+1; j <= n; ++j){
+            cin >> r[i][j];
+            m[i][j] = r[i][j];
+        }
+    }
+    rent();
+    cout << "花费最少租金为：" << m[i][n] << endl;
+    cout << "最少租金经过的站点为: " << 1;
+    print(1, n);
+    return 0;
+}
+
+//input 
+// 6
+// 2 6 9 15 20 3 5 11 18 3 6 12 5 8 6
